d03b8f8ee63a50c924f9a1d6bc59548e1b873c17
Lorenzo Pichetti <68168230+LorenzoPichetti@users.noreply.github.com>
2025-08-25 10:44:14 +0200

    Update mpi_macros.h
    Some preliminary MPI print utils

--- include/ccutils/mpi/mpi_macros.h
+++ include/ccutils/mpi/mpi_macros.h
@@ -4,7 +4,78 @@
 #error "ccutils MPI headers require -DCCUTILS_ENABLE_MPI"
 #endif
 
+#define MPI_ALL_PRINT(X) {                                                                                                              \
+    int inmacro_myid, inmacro_ntask;                                                                                                    \
+    MPI_Comm_rank(MPI_COMM_WORLD, &inmacro_myid);                                                                                       \
+	MPI_Comm_size(MPI_COMM_WORLD, &inmacro_ntask);                                                                                      \
+	\
+    FILE *fp;                                                                                                                           \
+    char s[50], s1[50];                                                                                                                 \
+    sprintf(s, "temp_%d.txt", inmacro_myid);                                                                                            \
+    fp = fopen ( s, "w" );                                                                                                              \
+    fclose(fp);                                                                                                                         \
+    fp = fopen ( s, "a+" );                                                                                                             \
+    fprintf(fp, "\t------------------------- Proc %d File %s Line %d -------------------------\n\n", inmacro_myid, __FILE__, __LINE__); \
+    X;                                                                                                                                  \
+    if (inmacro_myid==inmacro_ntask-1)                                                                                                  \
+        fprintf(fp, "\t--------------------------------------------------------------------------\n\n");                                \
+    fclose(fp);                                                                                                                         \
+    \
+    for (int i=0; i<inmacro_ntask; i++) {                                                                                               \
+        if (inmacro_myid == i) {                                                                                                        \
+            int error;                                                                                                                  \
+            sprintf(s1, "cat temp_%d.txt", inmacro_myid);                                                                               \
+            error = system(s1);                                                                                                         \
+            if (error == -1) fprintf(stderr, "Error at line %d of file %s\n", __LINE__, __FILE__);                                      \
+            sprintf(s1, "rm temp_%d.txt", inmacro_myid);                                                                                \
+            error = system(s1);                                                                                                         \
+            if (error == -1) fprintf(stderr, "Error at line %d of file %s\n", __LINE__, __FILE__);                                      \
+        }                                                                                                                               \
+        MPI_Barrier(MPI_COMM_WORLD);                                                                                                    \
+    }                                                                                                                                   \
+  }
 
+#define MPI_COMMUNICATOR_PRINT(CM, X)  \
+  {\
+    int inmacro_myid, inmacro_ntask;  \
+    MPI_Comm_rank(CM, &inmacro_myid);  \
+	MPI_Comm_size(CM, &inmacro_ntask);  \
+    FILE *fp;\
+    char s[50], s1[50];\
+    sprintf(s, "temp_%d.txt", inmacro_myid);\
+    fp = fopen ( s, "w" );\
+    fclose(fp);\
+    fp = fopen ( s, "a+" );\
+    fprintf(fp, "\t------------------------- Proc %d File %s Line %d -------------------------\n\n", inmacro_myid, __FILE__, __LINE__);\
+    X;\
+    if (inmacro_myid==inmacro_ntask-1) \
+        fprintf(fp, "\t--------------------------------------------------------------------------\n\n");\
+    fclose(fp);\
+    for (int i=0; i<inmacro_ntask; i++) {\
+        if (inmacro_myid == i) {\
+            int error; \
+            sprintf(s1, "cat temp_%d.txt", inmacro_myid);\
+            error = system(s1);\
+            if (error == -1) fprintf(stderr, "Error at line %d of file %s", __LINE__, __FILE__); \
+            sprintf(s1, "rm temp_%d.txt", inmacro_myid);\
+            error = system(s1);\
+            if (error == -1) fprintf(stderr, "Error at line %d of file %s", __LINE__, __FILE__); \
+        }\
+        MPI_Barrier(CM);\
+    }\
+  }
+
+#define MPI_PROCESS_PRINT(CM, P, X)  \
+  {\
+    int myid, ntask;  \
+    MPI_Comm_rank(CM, &myid);  \
+	MPI_Comm_size(CM, &ntask);  \
+	if (myid == P) {  \
+      fprintf(stdout, "\t--------------------- Proc %d of %d. File %s Line %d ---------------------\n\n", myid, ntask, __FILE__, __LINE__);\
+      X;\
+      fprintf(stdout, "\t--------------------------------------------------------------------------\n\n");\
+    }  \
+  }
 
 
 
