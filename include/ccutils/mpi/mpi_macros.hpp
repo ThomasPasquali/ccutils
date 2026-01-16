8553fbd3a170442b94d8a4d51df1ec0febe5b70e
JacopoRaffi <raffijacopo@gmail.com>
2026-01-16 09:03:14 +0100

    typo

--- include/ccutils/mpi/mpi_macros.hpp
+++ include/ccutils/mpi/mpi_macros.hpp
@@ -68,11 +68,11 @@
     for (int i=0; i<inmacro_ntask; i++) {                                            \
         if (ccutils_macro_myid == i) {                                               \
             int error;                                                               \
-            sprintf(s1, "cat ccutils_temp_%s_%d.txt", job_id, ccutils_macro_myidd);  \
+            sprintf(s1, "cat ccutils_temp_%s_%d.txt", job_id, ccutils_macro_myid);  \
             error = system(s1);                                                      \
             if (error != 0) fprintf(stderr, CCUTILS_FMT_ERROR,                       \
                 __LINE__, __FILE__, "MPI_ALL_PRINT: could not cat tmp file.");       \
-            sprintf(s1, "rm ccutils_temp_%s_%d.txt", job_id, ccutils_macro_myidd);   \
+            sprintf(s1, "rm ccutils_temp_%s_%d.txt", job_id, ccutils_macro_myid);   \
             error = system(s1);                                                      \
             if (error != 0) fprintf(stderr, CCUTILS_FMT_ERROR,                       \
                 __LINE__, __FILE__, "MPI_ALL_PRINT: could not rm tmp file.");        \
